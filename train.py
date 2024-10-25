6af68f3700bbe24d8718a2611dfd5c4fac1c9342
MarvinHill <86198682+MarvinHill@users.noreply.github.com>
2024-10-25 20:11:08 +0200

    split main file to train and predict and trained first custom model

---
+++ train.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+import cv2
+import matplotlib.pyplot as plt
+
+# Load Model
+model = YOLO('yolov10s.pt')
+
+
+model.train(
+    data='dataset.yaml',  # Pfad zur YAML-Datei, die den Datensatz beschreibt
+    epochs=100,  # Anzahl der Trainingsdurchläufe
+    imgsz=256,  # Bildgröße
+    batch=-1,  # Batch-Größe
+    name='custom_yolov10_model',  # Name des gespeicherten Modells
+    pretrained=True  # Verwenden von vortrainierten Gewichten (Transfer Learning)
+)
