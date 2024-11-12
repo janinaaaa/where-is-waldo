bd9a056570dad200b0ca5692e515ae078a08d014
Janina <101583496+janinaaaa@users.noreply.github.com>
2024-11-12 10:26:13 +0100

    added method for cutting images

--- crop_images.py
+++ crop_images.py
@@ -1,5 +1,5 @@
 import cv2
-
+import os, shutil
 
 # Function to crop the image
 def crop_image(image_path):
@@ -11,4 +11,47 @@ def crop_image(image_path):
     cv2.imwrite(image_path, image)
     print("Image cropped successfully")
 
+
+
+# write a fuction that takes an image and cuts in it smaller images of size 256x256
+def cut_image(image_path):
+    # Delete output folder if it exists
+    try:
+        shutil.rmtree("output")
+    except:
+        pass
+    os.mkdir("output")
+    # Read the image
+    image = cv2.imread(image_path)
+    # Get the dimensions of the image
+    height, width, _ = image.shape
+    # Define the size of the cropped images
+    crop_size = 256
+    imagename = image_path.split(".")[0]
+    # Loop over the image and crop it into smaller images
+    for i in range(0, height, crop_size):
+        for j in range(0, width, crop_size):
+            # Define the coordinates of the cropped image
+            x1, y1 = j, i
+            x2, y2 = j + crop_size, i + crop_size
+            # Crop the image
+            cropped_image = image[y1:y2, x1:x2]
+            # Save the cropped image in an output folder
+
+            cv2.imwrite(f"output/{imagename}{i}_{j}.jpg", cropped_image)
+    print("Image cropped successfully")
+
+    for i in range(128, height, crop_size):
+        for j in range(0, width, crop_size):
+            # Define the coordinates of the cropped image
+            x1, y1 = j, i
+            x2, y2 = j + crop_size, i + crop_size
+            # Crop the image
+            cropped_image = image[y1:y2, x1:x2]
+            # Save the cropped image in an output folder
+
+
+            cv2.imwrite(f"output/{imagename}_offset_{i}_{j}.jpg", cropped_image)
+
 crop_image("screenshot.jpg")
+cut_image("m.jpg")
