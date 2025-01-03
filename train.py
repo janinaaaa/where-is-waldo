632f673f0b1d5bf9527b030986d41dc58c4bd854
Janina <101583496+janinaaaa@users.noreply.github.com>
2025-01-03 14:21:08 +0100

    updated file for screenshot in presentation

--- train.py
+++ train.py
@@ -1,16 +1,13 @@
 from ultralytics import YOLO
-import cv2
-import matplotlib.pyplot as plt
 
 # Load Model
 model = YOLO('yolov10s.pt')
 
-
 model.train(
-    data='dataset.yaml',  # Pfad zur YAML-Datei, die den Datensatz beschreibt
-    epochs=100,  # Anzahl der Trainingsdurchläufe
-    imgsz=256,  # Bildgröße
+    data='dataset.yaml',  # Dataset
+    epochs=100,
+    imgsz=256,  # Image Size
     batch=-1,  # Batch-Größe
-    name='custom_yolov10_model',  # Name des gespeicherten Modells
-    pretrained=True  # Verwenden von vortrainierten Gewichten (Transfer Learning)
+    name='custom_yolov10_model',  # Name of trained model
+    pretrained=True  # Pretrained Weights (Transfer Learning)
 )
